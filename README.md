c71544041670e5af70d9b6368bf70ff43359cd3e
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2020-07-04 21:36:43 +0200

    Added Comments and Likes

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 A social network for animal shelters to find new owners of their temporary guests.
 
+### Tech Stack
+
+The application uses [ReactJS]() on the frontend and [NodeJS]() on the backend. As a database it uses [Postgres SQL]().
+
 Follow Me on Twitter for more updates
 
 -   [IchBinJustus](https://twitter.com/ichbinjustus)
--- data.sql
+++ data.sql
@@ -1,4 +1,6 @@
-DROP TABLE photos;
+DROP TABLE likes;
+DROP TABLE comments;
+DROP TABLE animals;
 DROP TABLE users;
 
 -- Create the table in the specified schema
@@ -16,5 +18,21 @@ CREATE TABLE animals
     image_url VARCHAR(255) not null,
     type_text VARCHAR(255),
     location_text VARCHAR(255),
+    created_at TIMESTAMP not null default now()
+);
+
+CREATE TABLE comments
+(
+    id BIGSERIAL not null PRIMARY KEY,
+    comment_text VARCHAR(255) not null,
+    user_id BIGINT REFERENCES users (id) not null,
+    animal_id BIGINT REFERENCES animals (id) not null,
+    created_at TIMESTAMP not null default now()
+);
+
+CREATE TABLE likes
+(
+    user_id BIGINT REFERENCES users (id) not null PRIMARY KEY,
+    animal_id BIGINT REFERENCES animals (id) not null,
     created_at TIMESTAMP not null default now()
 );
